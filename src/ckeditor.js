f573162a98b6826a0f50a15e3d83bd66495e8ffa
Zelkova <keithfgilbert@gmail.com>
2023-03-14 17:43:30 -0500

    removed watchdog

--- src/ckeditor.js
+++ src/ckeditor.js
@@ -53,7 +53,6 @@ import TextTransformation from '@ckeditor/ckeditor5-typing/src/texttransformatio
 import TodoList from '@ckeditor/ckeditor5-list/src/todolist';
 import Underline from '@ckeditor/ckeditor5-basic-styles/src/underline.js';
 import WordCount from '@ckeditor/ckeditor5-word-count/src/wordcount.js';
-import EditorWatchdog from '@ckeditor/ckeditor5-watchdog/src/editorwatchdog.js';
 
 class Editor extends BalloonBlockEditor {}
 
@@ -169,4 +168,4 @@ Editor.defaultConfig = {
 	}
 };
 
-export default { Editor, EditorWatchdog };
+export default Editor;
